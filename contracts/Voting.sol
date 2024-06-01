10d0753c315e8f53f497e446ea156e54010ff29f
Alkotashi <47549273+Alkotashi@users.noreply.github.com>
2024-06-01 06:06:20 +0300

    implemented function

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,65 +1,48 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @title A simple voting contract.
- */
 contract VotingContract {
-    // Struct for holding individual vote details.
     struct Vote {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Struct for holding voter details.
     struct Voter {
         bool authorized;
         bool voted;
         uint voteId;
     }
 
-    // State variables.
     address public owner;
     string public votingName;
     mapping(address => Voter) public voters;
     Vote[] public votes;
 
-    // Modifier to restrict access to the owner.
+    event VoteAdded(uint id, string name);
+    event VoterAuthorized(address voter);
+    event Voted(address voter, uint voteId);
+    event NewVotingSessionStarted(string newVotingName);
+
     modifier ownerOnly() {
         require(msg.sender == owner, "Only the owner can call this.");
         _;
     }
 
-    /**
-     * @dev Constructor for initializing the VotingContract.
-     * @param _name Name of the voting event.
-     */
     constructor(string memory _name) {
         owner = msg.sender;
         votingName = _name;
     }
 
-    /**
-     * @dev Adds a vote option.
-     * @param _name Name of the vote option.
-     */
     function addVote(string memory _name) external ownerOnly {
         votes.push(Vote(votes.length, _name, 0));
+        emit VoteAdded(votes.length - 1, _name);
     }
 
-    /**
-     * @dev Authorizes a voter.
-     * @param _person Address of the voter to authorize.
-     */
     function authorize(address _person) external ownerOnly {
         voters[_person].authorized = true;
+        emit VoterAuthorized(_person);
     }
 
-    /**
-     * @dev Records a vote for an authorized voter.
-     * @param _voteIndex Index of the vote being cast.
-     */
     function vote(uint _voteIndex) external {
         require(!voters[msg.sender].voted, "You already voted.");
         require(voters[msg.sender].authorized, "You are not authorized.");
@@ -70,33 +53,35 @@ contract VotingContract {
         voter.voteId = _voteIndex;
         voter.voted = true;
         votes[_voteIndex].voteCount++;
+
+        emit Voted(msg.sender, _voteIndex);
+    }
+    
+    function restartVotingSession(string memory _newVotingName) public ownerOnly {
+        for (uint i = 0; i < votes.length; i++) {
+            delete votes[i];
+        }
+        delete votes;
+        
+        for (uint i = 0; i < votes.length; i++) {
+            address voterAddress = address(uint160(i));
+            delete voters[voterAddress];
+        }
+        
+        votingName = _newVotingName;
+        emit NewVotingSessionStarted(_newVotingName);
     }
 
-    /**
-     * @dev Returns total votes for a given vote option.
-     * @param _voteIndex Index of the vote option.
-     * @return Total number of votes.
-     */
     function totalVotesFor(uint _voteIndex) external view returns (uint) {
         require(_voteIndex < votes.length, "No such vote.");
         return votes[_voteIndex].voteCount;
     }
 
-    /**
-     * @dev Fetches details of a vote option.
-     * @param _voteIndex Index of the vote option.
-     * @return Tuple containing vote details (id, name, voteCount).
-     */
     function getVote(uint _voteIndex) external view returns (uint, string memory, uint) {
         require(_voteIndex < votes.length, "No such vote.");
         return (votes[_voteIndex].id, votes[_voteIndex].name, votes[_voteIndex].voteCount);
     }
 
-    /**
-     * @dev Returns the details of a voter.
-     * @param _voter Address of the voter.
-     * @return Tuple containing the authorization status, voting status, and voted option index.
-     */
     function voterDetails(address _voter) external view returns (bool, bool, uint) {
         Voter memory voter = voters[_voter];
         return (voter.authorized, voter.voted, voter.voteId);
