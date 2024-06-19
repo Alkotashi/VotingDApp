fa0781db7859cfece5ce6c8e90a0cd991975afdc
Alkotashi <47549273+Alkotashi@users.noreply.github.com>
2024-06-19 16:50:47 +0300

    clean code

--- Models/Vote.cs
+++ Models/Vote.cs
@@ -4,20 +4,23 @@ using System.ComponentModel.DataAnnotations;
 public class Vote
 {
     public int Id { get; set; }
-    [Required]
-    [StringLength(100)]
+    
+    [Required, StringLength(100)]
     public string Title { get; set; }
+    
     [StringLength(500)]
     public string Description { get; set; }
+    
     public List<VoteOption> Options { get; set; } = new List<VoteOption>();
 }
 
 public class VoteOption
 {
     public int Id { get; set; }
-    [Required]
-    [StringLength(200)]
+    
+    [Required, StringLength(200)]
     public string Text { get; set; }
-    public int VoteId { get; set; }
+    
+    public int VoteId { get; show; set; }
     public Vote Vote { get; set; }
 }
